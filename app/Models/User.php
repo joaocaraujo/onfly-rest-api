ee3257cb9de6f0dd72d90e7d97463b572b007a26
Joao Araujo <araujo_joaol@yahoo.com.br>
2024-07-06 13:30:55 -0300

    feat: Implementation of migrations and models. JWT configuration for authentication.

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get the expenses for the user.
+     */
+    public function expenses()
+    {
+        return $this->hasMany(Expenses::class);
+    }
 }
