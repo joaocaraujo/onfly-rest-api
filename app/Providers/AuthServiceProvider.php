44a7ed2b018806ada23046054ca3c11d43c2be1d
Joao Araujo <araujo_joaol@yahoo.com.br>
2024-07-06 20:30:34 -0300

    chore(policy): configuring AuthServiceProvider for expenses

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use App\Policies\ExpensesPolicy;
+use App\Models\Expenses;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        Expenses::class => ExpensesPolicy::class,
     ];
 
     /**
